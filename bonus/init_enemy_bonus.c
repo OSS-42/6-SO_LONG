557115ed14efe571d4d582d481f12a8824adb970
xDeadpoolx <eric.wursteisen@gmail.com>
2022-07-27 16:32:36 -0400

    update so_long essaye animation patrouille

--- bonus/init_enemy_bonus.c
+++ bonus/init_enemy_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 15:22:53 by ewurstei          #+#    #+#             */
-/*   Updated: 2022/07/27 14:28:10 by ewurstei         ###   ########.fr       */
+/*   Updated: 2022/07/27 16:31:30 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 //x et y dans l'image sont inversés dans un tableau.
 //donc dans img->map[x][y] (classique), x = img->player_y et y = img->player_x
 //attention au 0 dans les 2 cas.
+
+void	animated_enemy(t_data *img)
+{
+	int	img_width;
+	int	img_height;
+	int	x;
+
+	x = 0;	
+	while (x < 5000)
+	{
+		img->enemy = mlx_xpm_file_to_image(img->mlx, "assets/patrouille.xpm",
+			&img_width, &img_height);
+		sleep(5);
+		img->enemy = mlx_xpm_file_to_image(img->mlx, "assets/patrouille2.xpm",
+			&img_width, &img_height);
+		sleep(5);
+		x++;
+	}
+}
 
 static void random_start(t_data *img)
 {
